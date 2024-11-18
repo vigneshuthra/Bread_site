9328297316372528931cce932142d46946df13b3
Vignesh Uthrapathy <52934902+vigneshuthra@users.noreply.github.com>
2024-11-18 23:31:15 +0000

    inital commit

---
+++ src/app/components/OurBreads.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OurBreads = () => {
+  return (
+    <div>OurBreads</div>
+  )
+}
+
+export default OurBreads
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      container:{
+        center:true,
+      }
     },
   },
   plugins: [],
